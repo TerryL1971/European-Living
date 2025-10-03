9edd8f8b47c5f58287da6b796343cbc920dcd0e2
TerryL1971 <tcl71771@yahoo.com>
2025-10-04 01:28:57 +0200

    fix: update homepage URL in package.json and adjust base path in vite.config.ts

--- european-living/vite.config.ts
+++ european-living/vite.config.ts
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  base: "/European-Living/",
+  base: "/", // important for custom domains
   plugins: [react(), tailwindcss()],
 });
